994dd507f4c1e70e78c229e8375ee1961f20cfe4
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-04-11 00:58:31 -0500

    primer commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,40 @@
+import './mylabel.css';
+
+export interface MyLabelProps {
+  /**
+   * Este es el mensaje a mostrar en la etiqueta
+   */
+  label: string;
+  /**
+   * Este es el tamaño de la etiqueta
+   */
+  size: 'normal' | 'h1' | 'h2' | 'h3';
+  /**
+   * si quiere todo capitalizado
+   */
+  allCaps?: boolean;
+  /**
+   * Colores basicos del boton
+   */
+  color?: 'primary' | 'secondary' | 'tertiary';
+  /**
+   * Colores personalizado de la fuente
+   */
+  fontColor?: string;
+}
+
+export const MyLabel = ({
+  allCaps = false,
+  color = 'primary',
+  label = 'No label',
+  size = 'normal',
+  fontColor,
+}: MyLabelProps) => {
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{ color: fontColor }}>
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
